9b3099be5cadf8492977a34fe362ba0e918a57ad
Saxon(木子朗) <lishuaishuai.it@gmail.com>
2020-10-16 13:35:17 +0000

    refactor: test

--- packages/issues-to-data/src/index.ts
+++ packages/issues-to-data/src/index.ts
@@ -1,1 +1,11 @@
-console.log(process.env.ACCESS_TOKEN)
+import { Octokit } from '@octokit/core';
+
+(async () => {
+  const octokit = new Octokit({
+    auth: process.env.ACCESS_TOKEN,
+    baseUrl: 'https://api.github.com/repos/zerolab-fe/awesome-nodejs',
+  });
+
+  const response = await octokit.request('GET /issues');
+  console.log("response", response)
+})();
